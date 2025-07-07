a1e1b5d8911fb524adf453535be7915b99ce7acb
Abhinav Biswal <iamabhinavbiswal@gmail.com>
2025-07-07 16:42:52 +0530

    Add files via upload

---
+++ Week - 3 HandsOn (Entity Framework Core)/EF_Core-Lab_Solutions/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=EFLabDB;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+}
